bffa47f3e1ff7afcae5d6ce28890858edd3cf280
rmdevv <michelonriccardo@gmail.com>
2024-03-07 12:57:19 +0100

    Fix: Aggiunto supporto per documenti docx

--- 3 - PB/MVP/src/backend/adapter/_in/web/upload_documents_controller.py
+++ 3 - PB/MVP/src/backend/adapter/_in/web/upload_documents_controller.py
@@ -1,17 +1,8 @@
-from datetime import datetime, timezone
 from typing import List
 
 from adapter._in.web.new_document import NewDocument
 from application.port._in.upload_documents_use_case import UploadDocumentsUseCase
-from domain.document.document import Document
-from domain.document.document_content import DocumentContent
-from domain.document.document_id import DocumentId
-from domain.document.document_metadata import DocumentMetadata
-from domain.document.document_metadata import DocumentType
 from domain.document.document_operation_response import DocumentOperationResponse
-from domain.document.document_status import DocumentStatus
-from domain.document.document_status import Status
-from domain.document.plain_document import PlainDocument
 
 """
 This class is the controller for the upload documents use case. It receives the new documents and converts them to the domain model.
@@ -28,22 +19,7 @@ class UploadDocumentsController:
     def __init__(self, upload_documents_use_case: UploadDocumentsUseCase):
         self.upload_documents_use_case = upload_documents_use_case
 
-    def toDocument(self, newDocument: NewDocument) -> Document:
-        documentType = DocumentType.PDF if newDocument.type.upper() == "PDF" else DocumentType.DOCX
-        return Document(
-            DocumentStatus(Status.ENABLED),
-            PlainDocument(
-                DocumentMetadata(
-                    DocumentId(newDocument.documentId),
-                    documentType,
-                    newDocument.size,
-                    datetime.now(timezone.utc)
-                ),
-                DocumentContent(newDocument.content)
-            )
-        )
-
     def uploadDocuments(self, newDocuments: List[NewDocument], forceUpload: bool = False) -> List[DocumentOperationResponse]:
-        documents = [self.toDocument(newDocument) for newDocument in newDocuments]
+        documents = [newDocument.toDocument() for newDocument in newDocuments]
         return self.upload_documents_use_case.uploadDocuments(documents, forceUpload)
     
--- 3 - PB/MVP/src/backend/blueprints/upload_documents.py
+++ 3 - PB/MVP/src/backend/blueprints/upload_documents.py
@@ -1,4 +1,5 @@
-from flask import request, Blueprint, jsonify
+import os
+from flask import request, Blueprint, jsonify, abort
 from adapter._in.web.new_document import NewDocument
 from adapter._in.web.upload_documents_controller import UploadDocumentsController
 from adapter.out.persistence.aws.AWS_manager import AWSS3Manager
@@ -13,20 +14,33 @@ from adapter.out.upload_documents.embeddings_uploader_vector_store import Embedd
 from adapter.out.upload_documents.huggingface_embedding_model import HuggingFaceEmbeddingModel
 from adapter.out.persistence.vector_store.vector_store_chromaDB_manager import VectorStoreChromaDBManager
 from adapter.out.persistence.vector_store.vector_store_pinecone_manager import VectorStorePineconeManager
+from werkzeug.utils import secure_filename
 
 uploadDocumentsBlueprint = Blueprint("uploadDocuments", __name__)
 
 @uploadDocumentsBlueprint.route("/uploadDocuments", methods=['POST'])
 def uploadDocuments():
     # TODO: Add validation for the request
-    newDocuments = [
-        NewDocument(
-            documentId = uploadedDocument.filename,
-            type = "PDF" if uploadedDocument.content_type == "application/pdf" else "DOCX",
-            size = uploadedDocument.content_length,
-            content = uploadedDocument.read()
-        ) for uploadedDocument in request.files.getlist('documents')
-    ]
+    newDocuments = []
+
+    for uploadedDocument in request.files.getlist('documents'):
+        filename = secure_filename(uploadedDocument.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext.lower() not in [".pdf", ".docx"]:
+                abort(400, f"Il documento {uploadedDocument.filename} non è supportato.")
+
+            contentType = uploadedDocument.content_type
+            if contentType == "application/pdf" or contentType == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                newDocument = NewDocument(
+                    documentId = uploadedDocument.filename,
+                    type = "PDF" if contentType == "application/pdf" else "DOCX",
+                    size = uploadedDocument.content_length,
+                    content = uploadedDocument.read()
+                )
+                newDocuments.append(newDocument)
+        else:
+            abort(400, "L'upload di documenti senza titolo non è supportato.")
 
     controller = UploadDocumentsController(
         UploadDocumentsService(
@@ -42,7 +56,7 @@ def uploadDocuments():
                         HuggingFaceEmbeddingModel()
                     ),
                     EmbeddingsUploaderVectorStore(
-                        VectorStorePineconeManager()
+                        VectorStoreChromaDBManager()
                     )
                 )
             )

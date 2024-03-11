936ff974eeadcaa22bf782ce3edc20d2ccb9855a
GianlucaBresolin <117901582+GianlucaBresolin@users.noreply.github.com>
2024-03-11 10:33:42 +0100

    Feat: delete chats.

---
+++ 3 - PB/MVP/src/backend/application/port/_in/delete_chats_use_case.py
@@ -0,0 +1,7 @@
+from typing import List
+from domain.chat.chat_operation_response import ChatOperationResponse   
+from domain.chat.chat_id import ChatId
+
+class DeleteChatUseCase:
+    def deleteChats(self, chatsIdsList: List[ChatId]) -> List[ChatOperationResponse]:
+        pass

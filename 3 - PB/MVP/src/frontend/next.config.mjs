3ba098756e7c5b9a1605c68db747cbf95370135e
rmdevv <michelonriccardo@gmail.com>
2024-03-17 11:19:16 +0100

    Feat: Aggiunto document viewer

--- 3 - PB/MVP/src/frontend/next.config.mjs
+++ 3 - PB/MVP/src/frontend/next.config.mjs
@@ -14,44 +14,12 @@ const nextConfig = {
             },
         ],
     },
-    redirects() {
-        return [
-            {
-                source: '/components',
-                destination: '/docs/components/accordion',
-                permanent: true,
-            },
-            {
-                source: '/docs/components',
-                destination: '/docs/components/accordion',
-                permanent: true,
-            },
-            {
-                source: '/examples',
-                destination: '/examples/mail',
-                permanent: false,
-            },
-            {
-                source: '/docs/primitives/:path*',
-                destination: '/docs/components/:path*',
-                permanent: true,
-            },
-            {
-                source: '/figma',
-                destination: '/docs/figma',
-                permanent: true,
-            },
-            {
-                source: '/docs/forms',
-                destination: '/docs/components/form',
-                permanent: false,
-            },
-            {
-                source: '/docs/forms/react-hook-form',
-                destination: '/docs/components/form',
-                permanent: false,
-            },
-        ]
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\bcanvas\.(node)/,
+            use: 'raw-loader',
+        })
+        return config
     },
 }
 
--- 3 - PB/MVP/src/frontend/src/app/documents/document/page.tsx
+++ 3 - PB/MVP/src/frontend/src/app/documents/document/page.tsx
@@ -3,31 +3,15 @@
 import React from "react";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
-// custom loader
-// const MyLoadingRenderer = ({ document, fileName }) => {
-//   const fileText = fileName || document?.fileType || "";
-
-//   if (fileText) {
-//     return <div>Loading Renderer ({fileText})...</div>;
-//   }
-
-//   return <div>Loading Renderer...</div>;
-// };
-
-function Home({ }) {
-    // for docx, pptx & xlsx, you need to upload in any cloud platform it could be amazon s3, github and all. But you need to make sure there is no cors policy. for more info visit this link: https://www.npmjs.com/package/@cyntler/react-doc-viewer
+export function DocumentViewer() {
     const docs = [
         {
-            uri: "ANDREABARUTTA_322556_0.pdf", fileType: 'pdf', // for local file
-        },
-        {
-            uri: "Sistemi_Ripasso.docx", fileType: 'docx', // TODO: docx possono essere solo link pubblici perché il renderer usa un servizio web che necessita di accedere al documento
+            uri: "", fileType: 'pdf',
         }
     ];
     return (
         <div>
             <DocViewer
-                prefetchMethod="GET" // for remote fetch
                 documents={docs}
                 pluginRenderers={DocViewerRenderers}
             // style={{ width: "50%" }} //custom style
@@ -40,5 +24,3 @@ function Home({ }) {
         </div>
     );
 }
-
-export default Home;

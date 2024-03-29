88cc3b31c7fa0a5a670574df924f92c194a42589
dugoalberto <www.da.0502@gmail.com>
2024-03-29 11:18:23 +0100

    Fix: Aggiornamento import

--- 3 - PB/MVP/tests/backend/system/change_configuration_system_test.py
+++ 3 - PB/MVP/tests/backend/system/change_configuration_system_test.py
@@ -1,12 +1,11 @@
 from unittest.mock import MagicMock, patch, ANY
 
-from _in.web.change_configuration_controller import ChangeConfigurationController
+from adapter._in.web.change_configuration_controller import ChangeConfigurationController
 from domain.configuration.configuration_operation_response import ConfigurationOperationResponse
-from out.change_configuration.change_configuration_postgres import ChangeConfigurationPostgres
-from out.persistence.postgres.postgres_configuration_operation_response import PostgresConfigurationOperationResponse
-from out.persistence.postgres.postgres_configuration_orm import PostgresConfigurationORM
-from service.change_configuration_service import ChangeConfigurationService
-from service.set_configuration_service import SetConfigurationService
+from adapter.out.change_configuration.change_configuration_postgres import ChangeConfigurationPostgres
+from adapter.out.persistence.postgres.postgres_configuration_operation_response import PostgresConfigurationOperationResponse
+from adapter.out.persistence.postgres.postgres_configuration_orm import PostgresConfigurationORM
+from application.service.change_configuration_service import ChangeConfigurationService
 
 def test_changeLLMModel():
     with     patch('adapter.out.persistence.postgres.postgres_configuration_orm.db_session') as db_session, \
@@ -14,7 +13,6 @@ def test_changeLLMModel():
             osEnvironMock.get.return_value = 1
             LLModel = "OPENAI"
             PostgresConfigurationORM.changeLLMModel.return_value = PostgresConfigurationOperationResponse(True, "Modello LLM cambiato con successo")
-            #db_sessionMock.query.return_value.filter.return_value.update.return_value = None
 
             controller = ChangeConfigurationController(ChangeConfigurationService(ChangeConfigurationPostgres(PostgresConfigurationORM())))
             result = controller.changeLLMModel(LLModel)
